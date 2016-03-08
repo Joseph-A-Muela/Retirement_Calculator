af2132da8f9b0aed70786d4ef8f689e1869b49fa
Joseph-A-Muela <josephamuela@gmail.com>
2016-03-07 22:19:57 -0600

    Completed Explorer Mode of the paint calc assignment.

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,8 @@ document.write('<div>You are '+currentAge+' years old.</div>');
 var retireAge = prompt('At what age do you want to retie?');
 document.write('<div>You want to be '+retireAge+' years old when you retire.</div>')
 
-var retirment = parseFloat(retireAge - currentAge);
-var retireYear = new Date().getFullYear() + retirment;
+var retirement = parseFloat(retireAge - currentAge);
+var retireYear = new Date().getFullYear() + retirement;
 
 document.write('<div>You want to retire in '+retireYear+'.</div>');
 console.log('You want to retire in '+retireYear+' years.');
